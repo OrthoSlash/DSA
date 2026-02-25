f0337c60e5942165af59db313f5e61c89641a2e7
BunnyBro <devanshnakrani2@gmail.com>
2026-02-25 22:13:40 +0530

    Day 6:4 Sum

---
+++ Array/4Sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        if(nums.size() < 4) return {};
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans1;
+        for(int i=0;i<nums.size()-3;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1;j<nums.size()-2;j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int left=j+1,right=nums.size()-1;
+                while(right>left){
+                    long long ans=(long long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(ans==target){
+                        ans1.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        while(left < right && nums[left] == nums[left+1]) left++;
+                        while(left < right && nums[right] == nums[right-1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if(ans<target) left++;
+                    else right--;
+                }
+            }
+        }
+        return ans1;
+    }
+};
